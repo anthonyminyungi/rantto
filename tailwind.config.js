28fc08631c7f25eed37e243f0e399cf2a44a9431
anthonymin <dbstnsdl12@naver.com>
2023-10-09 15:48:48 +0900

    feat: 여백 Spacer 컴포넌트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { spacing } from "./src/constants";
+
+/* TODO: 임시 작성, 추후 개선 고민 */
+const spaceSafeList = [
+  ...spacing.map((v) => `w-${v}`),
+  ...spacing.map((v) => `h-${v}`),
+];
+
 export default {
+  safelist: [...spaceSafeList],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
