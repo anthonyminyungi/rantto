dbbb60a90a2c4a48253cd6240e9d4b4c4df9858c
anthonymin <dbstnsdl12@naver.com>
2023-12-10 00:58:59 +0900

    fix: winning history update cron function 오류 수정

--- api/winningHistory.ts
+++ api/winningHistory.ts
@@ -4,6 +4,11 @@ import { parse } from "node-html-parser";
 import { format } from "date-fns";
 import stringify from "json-stringify-pretty-compact";
 
+export const config = {
+  runtime: "nodejs",
+  regions: ["icn1"],
+};
+
 export default async function handler(
   _req: VercelRequest,
   res: VercelResponse
@@ -15,11 +20,13 @@ export default async function handler(
     });
 
     const owner = "anthonyminyungi";
+    const ownerEmail = "dbstnsdl12@naver.com";
     const repo = "rantto";
     const filePath = "src/assets/winning_history.json";
     const message = "Update winning_history.json";
     const develop = "develop";
     const main = "main";
+    const tempBranch = "functions/update-winning-history";
     const dhlotteryUrl = "https://dhlottery.co.kr/gameResult.do?method=byWin";
 
     /* 이번주 당첨번호 크롤링 */
@@ -36,21 +43,36 @@ export default async function handler(
       (root.querySelector(".win_result strong")?.text || "").slice(0, 4),
       10
     );
-
     console.log("data : ", round, numbers, bonus);
+
+    const { data: getRef } = await octokit.git.getRef({
+      owner,
+      repo,
+      ref: `heads/${develop}`,
+    });
+    const developSha = getRef.object.sha;
+    console.log(developSha);
+
+    const { data: createRef } = await octokit.rest.git.createRef({
+      owner,
+      repo,
+      ref: `refs/heads/${tempBranch}`,
+      sha: developSha,
+    });
+    console.log("create ref : ", createRef);
 
     /* 기존 당첨번호 데이터 접근 */
     const { data: getContent } = await octokit.repos.getContent({
       owner,
       repo,
       path: filePath,
-      ref: main,
+      ref: `heads/${tempBranch}`,
     });
     let json;
-    let sha = "";
+    let getContentSha = "";
     if (!Array.isArray(getContent) && getContent.type === "file") {
       json = JSON.parse(atob(getContent.content));
-      sha = getContent.sha;
+      getContentSha = getContent.sha;
     }
     /* 이번주 당첨번호 추가 */
     json.history.push({
@@ -59,27 +81,43 @@ export default async function handler(
       numbers,
       createdAt: format(new Date(), "yyyy-MM-dd"),
     });
-
     console.log("json : ", json);
 
-    /* develop 브랜치에 추가된 데이터 반영 update commit */
+    /* 임시 브랜치에 추가된 데이터 반영 update commit */
     const { data: updateFile } = await octokit.repos.createOrUpdateFileContents(
       {
         owner,
         repo,
-        branch: develop,
+        branch: tempBranch,
         path: filePath,
         message,
         content: btoa(stringify(json)),
-        sha,
+        sha: getContentSha,
         committer: {
           name: owner,
-          email: "dbstnsdl12@naver.com",
+          email: ownerEmail,
         },
       }
     );
-
     console.log("commit id : ", updateFile.commit.sha);
+
+    /* 임시 브랜치 -> develop pull request 생성 */
+    const { data: createFilePR } = await octokit.rest.pulls.create({
+      owner,
+      repo,
+      head: tempBranch,
+      base: develop,
+      title: message,
+    });
+    console.log("file pr number : ", createFilePR.number);
+
+    /* 생성된 pull request 병합 */
+    const { data: mergeFilePR } = await octokit.rest.pulls.merge({
+      owner,
+      repo,
+      pull_number: createFilePR.number,
+    });
+    console.log("file pr merged : ", mergeFilePR.merged);
 
     /* develop -> master pull request 생성 */
     const { data: createPR } = await octokit.rest.pulls.create({
@@ -89,7 +127,6 @@ export default async function handler(
       base: main,
       title: message,
     });
-
     console.log("pr number : ", createPR.number);
 
     /* 생성된 pull request 병합 */
@@ -98,14 +135,8 @@ export default async function handler(
       repo,
       pull_number: createPR.number,
     });
-
     console.log("merged : ", mergePR.merged);
 
-    if (!mergePR.merged) {
-      res.statusCode = 500;
-      res.send({ message: mergePR.message });
-      return;
-    }
     res.statusCode = 200;
     res.send({ message: "ok" });
   } catch (e) {
