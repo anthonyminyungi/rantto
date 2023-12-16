717c63f9dc0f37d3794637a37d5543863ea83001
anthonyminyungi <dbstnsdl12@naver.com>
2023-12-17 02:10:16 +0900

    fix: add optional for undefined json

--- scripts/delete_preview.mjs
+++ scripts/delete_preview.mjs
@@ -1,11 +1,11 @@
 import { vercelApi } from "./vercel.mjs";
 
 // get preview deployments
-const { deployments } = await vercelApi("/v6/deployments?target=preview");
+const json = await vercelApi("/v6/deployments?target=preview");
 
 // delete deployment for each
 await Promise.all(
-  deployments.map(async (deployment) => {
+  json?.deployments?.map(async (deployment) => {
     await vercelApi(`/v13/deployments/${deployment.uid}`, { method: "delete" });
     console.log(`Deleted ${deployment.uid} successfully.`);
   })
--- scripts/delete_production.mjs
+++ scripts/delete_production.mjs
@@ -1,11 +1,11 @@
 import { vercelApi } from "./vercel.mjs";
 
 // get production deployments
-const { deployments } = await vercelApi("/v6/deployments?target=production");
+const json = await vercelApi("/v6/deployments?target=production");
 
 // delete deployment for each
 await Promise.all(
-  deployments.map(async (deployment) => {
+  json?.deployments?.map(async (deployment) => {
     const now = Math.round(new Date() / 1000);
     // if deployed 2 weeks or more ago
     if (now - deployment.ready > 2 * 7 * 24 * 60 * 60) {
