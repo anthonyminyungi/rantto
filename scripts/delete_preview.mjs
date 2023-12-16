7e1533b8c8e1c1e0713e7eb88a8d2558b4beacb8
anthonyminyungi <dbstnsdl12@naver.com>
2023-12-17 02:14:41 +0900

    fix: add log to test

--- scripts/delete_preview.mjs
+++ scripts/delete_preview.mjs
@@ -3,6 +3,10 @@ import { vercelApi } from "./vercel.mjs";
 // get preview deployments
 const json = await vercelApi("/v6/deployments?target=preview");
 
+console.log("json : ", json);
+
+if (!json) return;
+
 // delete deployment for each
 await Promise.all(
   json?.deployments?.map(async (deployment) => {
--- scripts/vercel.mjs
+++ scripts/vercel.mjs
@@ -1,4 +1,5 @@
 export const vercelApi = async (path, options = {}) => {
+  console.log("token : ", process.env.VERCEL_ACCESS_TOKEN);
   return fetch(`https://api.vercel.com${path}`, {
     headers: {
       ...options,
