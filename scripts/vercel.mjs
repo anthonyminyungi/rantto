b16704d59d155cfd885fc6ae28e3c4343aebaf0a
anthonymin <dbstnsdl12@naver.com>
2023-12-17 03:41:36 +0900

    fix: move options location

--- scripts/vercel.mjs
+++ scripts/vercel.mjs
@@ -1,8 +1,8 @@
 export const vercelApi = async (path, options = {}) => {
   console.log("token : ", process.env.VERCEL_ACCESS_TOKEN);
   return fetch(`https://api.vercel.com${path}`, {
+    ...options,
     headers: {
-      ...options,
       Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
       "Content-Type": "application/json",
     },
