0729da72ea1d157a46e3d52b6a8a676ae9ac2d20
anthonymin <dbstnsdl12@naver.com>
2023-12-25 23:50:46 +0900

    fix: 히스토리 수정용 ref를 main에서 따서 main으로 머지하도록 수정

--- api/winningHistory.ts
+++ api/winningHistory.ts
@@ -22,7 +22,6 @@ export default async function handler(
     const repo = "rantto";
     const filePath = "src/assets/winning_history.json";
     const message = "Update winning_history.json";
-    const develop = "develop";
     const main = "main";
     const tempBranch = "functions/update-winning-history";
     const dhlotteryUrl = "https://dhlottery.co.kr/gameResult.do?method=byWin";
@@ -46,16 +45,16 @@ export default async function handler(
     const { data: getRef } = await octokit.git.getRef({
       owner,
       repo,
-      ref: `heads/${develop}`,
+      ref: `heads/${main}`,
     });
-    const developSha = getRef.object.sha;
-    console.log(developSha);
+    const mainSha = getRef.object.sha;
+    console.log(mainSha);
 
     const { data: createRef } = await octokit.rest.git.createRef({
       owner,
       repo,
       ref: `refs/heads/${tempBranch}`,
-      sha: developSha,
+      sha: mainSha,
     });
     console.log("create ref : ", createRef);
 
@@ -99,31 +98,11 @@ export default async function handler(
     );
     console.log("commit id : ", updateFile.commit.sha);
 
-    /* 임시 브랜치 -> develop pull request 생성 */
-    const { data: createFilePR } = await octokit.rest.pulls.create({
-      owner,
-      repo,
-      head: tempBranch,
-      base: develop,
-      title: message,
-    });
-    console.log("file pr number : ", createFilePR.number);
-
-    await wait(3000);
-
-    /* 생성된 pull request 병합 */
-    const { data: mergeFilePR } = await octokit.rest.pulls.merge({
-      owner,
-      repo,
-      pull_number: createFilePR.number,
-    });
-    console.log("file pr merged : ", mergeFilePR.merged);
-
-    /* develop -> master pull request 생성 */
+    /* 임시 브랜치 -> main pull request 생성 */
     const { data: createPR } = await octokit.rest.pulls.create({
       owner,
       repo,
-      head: develop,
+      head: tempBranch,
       base: main,
       title: message,
     });
