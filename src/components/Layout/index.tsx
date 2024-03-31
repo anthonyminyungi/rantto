e1417a761937dda191dcf314573d86881df75b87
anthonymin <dbstnsdl12@naver.com>
2024-03-31 16:53:06 +0900

    feat: Toast container Portal 전환

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,25 +8,12 @@ import { entriesFromObject } from "@/utils";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div
-      /** child fixed element가 parent element의 box에 영향을 받도록 하려면
-       * transform, perspective, filter attribute가 필요
-       * https://developer.mozilla.org/ko/docs/Web/CSS/transform
-       */
-      className={cx(
-        "m-0",
-        "p-0",
-        "outline-none",
-        "bg-transparent",
-        /* sm */
-        "sm:rotate-0"
-      )}
-    >
+    <>
       <main className={cx("max-w-5xl", "mx-auto", "px-4", "py-2")}>
         <NavMenu tabs={entriesFromObject(MENU_TABS)} />
         {children}
       </main>
       <ToastList />
-    </div>
+    </>
   );
 }
